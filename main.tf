6fcdb002d1ce6a5ef1721fd1dfa46a87541981aa
Darwin Rinderer <darwin@helium.foundation>
2024-05-15 14:13:37 -0400

    Removing .terraform.lock.hcl

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_name       = "chirpstack_vpc"
+  vpc_name       = "chirpstack-vpc"
   vpc_cidr_block = "10.0.0.0/16"
 
   availability_zone_1 = "us-east-1a"
